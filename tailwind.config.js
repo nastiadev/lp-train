b8869f1060f52b0dd4c74c5da5b1a8b5fba1a719
nastiadev <agorodinskaya@live.com>
2023-06-23 16:13:12 +1000

    add icons structure page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,9 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    screens:{
-      sm:'480px',
-      md: '768px',
-      lg:'976px',
-      xl: '1440px'
-    },
     extend: {
       colors: {
-        btnColor: '#EBECF1',
-        btnBorderColor: '#4E4E4E',
-        btnFromColor: '#EBECF1',
-        btnViaColor: '#C0C4CE',
-        btnToColor: '#A9AEBB'
+        textColor:'#4e4e4e',
       },
     }
   },
