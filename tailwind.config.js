fb7c46b7106bd7a2c57aea3a2420ad3af4477037
nastiadev <agorodinskaya@live.com>
2023-06-23 10:58:35 +1000

    wip button postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,63 +10,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        pinkTest: '#FFC0CB',
         btnColor: '#EBECF1',
         btnBorderColor: '#4E4E4E',
         btnFromColor: '#EBECF1',
         btnViaColor: '#C0C4CE',
         btnToColor: '#A9AEBB'
       },
-      borderRadius: {
-        radius: '40px'
-      },
-      dropShadow: {
-        '3': '0 35px 35px rgba(0, 0, 0, 0.25)',
-      },
-      width: {
-        width: '150px',
-      },
-      height:{
-        height: '50px'
-      },
-      'animation': {
-            'gradient-x':'gradient-x 3s ease infinite',
-            'gradient-y':'gradient-y 3s ease infinite',
-            'gradient-xy':'gradient-xy 3s ease infinite',
-        },
-        'keyframes': {
-            'gradient-y': {
-                '0%': {
-                    'background-size':'400% 0%',
-                    'background-position': 'bottom right'
-                },
-                '100%': {
-                    'background-size':'0 0',
-                    'background-position': 'top left'
-                }
-            },
-            'gradient-x': {
-                '0%, 100%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            },
-            'gradient-xy': {
-                '0%, 100%': {
-                    'background-size':'400% 400%',
-                    'background-position': 'left center'
-                },
-                '50%': {
-                    'background-size':'200% 200%',
-                    'background-position': 'right center'
-                }
-            }
-        },
-      }
+    }
   },
   plugins: [],
 }
